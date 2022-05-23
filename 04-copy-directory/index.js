82d5349f75b317e1512dea69910ecd42ef37b226
TatianaYakushonok <yakush2@yandex.ru>
2022-05-23 13:58:23 +0700

    feat: add task 04-copy-directory (Mon, 23 May 2022 13:58:23 +0700)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const path = require('path');
 
+const dir = path.join(__dirname, 'files');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, function() {
+    fs.readdir(dir, (err, files) => {
+        if (err) {
+            console.error(err);
+            return
+        }
+        for (let file of files) {
+            fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
+                if (err) {
+                    console.error(err);
+                    return
+                }
+            });
+        }
+    })
+});
